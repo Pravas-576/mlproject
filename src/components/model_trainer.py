157618e03e6237b94ade781b079d15f94425a449
Pravas Kumar Panda <106006353+Pravas-576@users.noreply.github.com>
2023-04-08 23:59:01 +0530

    Model Trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -3,34 +3,35 @@ import sys
 from dataclasses import dataclass
 
 from catboost import CatBoostRegressor
-from sklearn.ensemble import(
+from sklearn.ensemble import (
     AdaBoostRegressor,
     GradientBoostingRegressor,
     RandomForestRegressor,
 )
-
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object,evaluate_model
+
+from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifacts","model.pkl")
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config = ModelTrainerConfig()
+        self.model_trainer_config=ModelTrainerConfig()
+
 
     def initiate_model_trainer(self,train_array,test_array):
-
         try:
-            logging.info("Split traiing and test input data")
-            X_train,y_train,X_test,y_test = (
+            logging.info("Split training and test input data")
+            X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
@@ -41,19 +42,24 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-            model_report = evaluate_model(X_train = X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
-
+            
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models)
+            
             ## To get best model score from dict
-            best_model_score = max(sorted(model_report.keys()))
+            best_model_score = max(sorted(model_report.values()))
 
-            ## TO get best model name from dict
+            ## To get best model name from dict
+
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
+
             if best_model_score<0.6:
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
@@ -67,6 +73,10 @@ class ModelTrainer:
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
+            
+
+
+
             
         except Exception as e:
             raise CustomException(e,sys)
